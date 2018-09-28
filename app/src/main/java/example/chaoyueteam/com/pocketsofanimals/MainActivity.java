3d4c152ee450e9258982ec4766d0305f49d21003
hedeqing <240484406@qq.com>
2018-09-28 20:18:31 +0800

    hdq

--- app/src/main/java/example/chaoyueteam/com/pocketsofanimals/MainActivity.java
+++ app/src/main/java/example/chaoyueteam/com/pocketsofanimals/MainActivity.java
@@ -2,6 +2,9 @@ package example.chaoyueteam.com.pocketsofanimals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.ib_profile);
     }
 }
