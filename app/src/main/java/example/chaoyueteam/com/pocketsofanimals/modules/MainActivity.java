9e0b1ed46f3136f6512072c9324fea71ef2e1f77
zhenghao964423063 <34414087+zhenghao964423063@users.noreply.github.com>
2018-10-03 14:44:29 +0800

    zh

--- app/src/main/java/example/chaoyueteam/com/pocketsofanimals/modules/MainActivity.java
+++ app/src/main/java/example/chaoyueteam/com/pocketsofanimals/modules/MainActivity.java
@@ -6,9 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
