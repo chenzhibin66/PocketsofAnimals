e8b1c8b4a8399f30f48bb5dd7a5f7ade9420e242
Julyyt <18636922817@163.com>
2018-10-04 14:00:46 +0800

    测试

--- app/src/main/java/example/chaoyueteam/com/pocketsofanimals/MainActivity.java
+++ app/src/main/java/example/chaoyueteam/com/pocketsofanimals/MainActivity.java
@@ -1,7 +1,14 @@
 package example.chaoyueteam.com.pocketsofanimals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.baidu.mapapi.map.MapView;
+
+import map.MapAcitvity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, MapAcitvity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
