54add3982b2071e9f38427258a9208a4cd1121ae
hedeqing <240484406@qq.com>
2018-10-07 17:01:46 +0800

    updates

--- app/src/main/java/example/chaoyueteam/com/pocketsofanimals/modules/takephoto/ShowAnimalsActivity.java
+++ app/src/main/java/example/chaoyueteam/com/pocketsofanimals/modules/takephoto/ShowAnimalsActivity.java
@@ -1,5 +1,7 @@
 package example.chaoyueteam.com.pocketsofanimals.modules.takephoto;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaPlayer;
@@ -16,12 +18,13 @@ import android.view.View;
 import android.widget.ImageView;
 
 import android.widget.Toast;
-
-
 import com.bumptech.glide.Glide;
 import android.widget.Toast;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import example.chaoyueteam.com.pocketsofanimals.R;
@@ -34,14 +37,18 @@ import example.chaoyueteam.com.pocketsofanimals.util.BitmapUtil;
 import example.chaoyueteam.com.pocketsofanimals.util.ZoomImageView;
 
 
+import static android.provider.MediaStore.Images.Media.insertImage;
 import static example.chaoyueteam.com.pocketsofanimals.image.AnimalDemo.getAnimalBean;
+import static example.chaoyueteam.com.pocketsofanimals.modules.takephoto.TakePhotoActivity.IMAGE_URI;
 
 public class ShowAnimalsActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "ShowAnimalsActivity";
     public static final String PATH = Environment.getExternalStorageDirectory().toString() + "/AndroidMedia/New_animal_picture/";
     Album album;
     private MediaPlayer mediaPlayer = new MediaPlayer();
+
     Animal animal;
     AlbumUtil albumUtil;
     Bitmap bitmap1;
@@ -88,11 +95,13 @@ public class ShowAnimalsActivity extends AppCompatActivity {
                     long dateTaken = System.currentTimeMillis();
 
                     String filename = DateFormat.format("yyyy-MM-dd kk.mm.ss", dateTaken).toString() + ".jpg";
+
                     String path_new = PATH+filename;
                     Uri uri = Uri.fromFile(file1);
                     bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                     BitmapUtil bitmapUtil = new BitmapUtil();
                     bitmap1 = bitmapUtil.translate(bitmap,text);
+                    Uri uri1 = insertImage(getContentResolver(), filename, dateTaken, PATH, filename, bitmap1, null);
                     bitmapUtil.saveBitmapFile(bitmap1,path_new);
 
                     Log.d("onCreate","成功");
@@ -140,6 +149,47 @@ public class ShowAnimalsActivity extends AppCompatActivity {
         });
         executorService.shutdown();
 
+    }
+
+    private Uri insertImage(ContentResolver cr, String name, long dateTaken,
+                            String directory, String filename, Bitmap source, byte[] jpegData) {
+        OutputStream outputStream = null;
+        String filePath = directory + filename;
+        try {
+            File dir = new File(directory);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            File file = new File(directory, filename);
+            if (file.createNewFile()) {
+                outputStream = new FileOutputStream(file);
+                if (source != null) {
+                    source.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                } else {
+                    outputStream.write(jpegData);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, e.getMessage());
+            return null;
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+            return null;
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (Throwable t) {
+                }
+            }
+        }
+        ContentValues values = new ContentValues(7);
+        values.put(MediaStore.Images.Media.TITLE, name);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename);
+        values.put(MediaStore.Images.Media.DATE_TAKEN, dateTaken);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.Images.Media.DATA, filePath);
+        return cr.insert(IMAGE_URI, values);
     }
     private void initMediaPlayer(String paths){
         File file =new File(paths);
